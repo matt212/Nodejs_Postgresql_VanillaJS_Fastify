3f6cf8533808d2fc906df06fd5f1b9dea085e255
Rizwan Patel <rizwan.patel@corp.intelliswift.com>
2020-12-05 18:54:57 +0530

    formatting and code cleanup

--- app/routes/emp_fastify.js
+++ app/routes/emp_fastify.js
@@ -14,12 +14,12 @@ async function routes(fastify, options) {
   fastify.get('/', async (request, reply) => {
     return { hello: 'world' }
   })
-  fastify.log.debug(dep.routeUrls.searchtype[0] + '------' + '/api/load/');
+  
 
   fastify.post(dep.routeUrls.searchtype[0], {
     preValidation: [fastify.authenticate]
   }, async (request, reply) => {
-    // fastify.log.debug(request.body);
+    
     dep.assignVariables(mod);
     var req = {}
 
@@ -28,7 +28,9 @@ async function routes(fastify, options) {
       return arg
     });
   })
-  fastify.post(dep.routeUrls.searchtype[1], async (request, reply) => {
+  fastify.post(dep.routeUrls.searchtype[1],{
+    preValidation: [fastify.authenticate]
+  }, async (request, reply) => {
     // fastify.log.debug(request.body);
     dep.assignVariables(mod);
     var req = {}
