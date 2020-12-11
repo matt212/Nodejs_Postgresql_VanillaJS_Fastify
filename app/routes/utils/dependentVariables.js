899756fd55298613f8a08a11e0430dda70396f72
Rizwan Patel <rizwan.patel@corp.intelliswift.com>
2020-12-11 14:53:50 +0530

    applied more modularity

--- app/routes/utils/dependentVariables.js
+++ app/routes/utils/dependentVariables.js
@@ -8,6 +8,7 @@ let redisMiddleware = require("../utils/redisMemcache");
 let Promises = require("bluebird");
 let crypto = require("crypto");
 let Queue = require("better-queue");
+var cGzip = require('../utils/misc/cGzip.js')
 let copyFrom = require("pg-copy-streams").from;
 /*let json2xls = require('json2xls');*/
 let async = require("async").parallel;
@@ -1868,6 +1869,7 @@ let baseUtilsRoutes = {
   SqlPivot: "SqlPivot"
 };
 module.exports = {
+  cGzip,
   baseUtilsRoutes,
   routeUrls,
   MemoryUsage,
