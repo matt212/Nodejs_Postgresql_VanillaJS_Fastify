5ea7ccae3553530c6146677b1b897718d7913217
Rizwan Patel <captainprice212@gmail.com>
2020-12-13 02:12:28 +0530

    Custom modular test suite approach applied

--- app/utils/test/Employees-test.spec.js
+++ app/utils/test/Employees-test.spec.js
@@ -1,86 +1,48 @@
-var supertest = require('supertest')
-//var should = require("should");
-
-var should = require('chai').should()
-let tokenvalEval
-before(async () => {
-  require('../app2.js')
-})
-
-// This agent refers to PORT where program is running.
-
-var server = supertest.agent('http://localhost:3011')
-
-// UNIT test begin
-
-let loginsuccess = function (
-  cred = { username: 'krennic', password: 'orson' }
-) {
-  return (promise = new Promise((resolve, reject) => {
-    var user = cred
-    server
-      .post('/login')
-      .send(user)
-      .expect('Content-type', /json/)
-      .expect(200)
-      .end(function (err, res) {
-        if (err) {
-          resolve({ status: 'fail', content: err })
-        }
-
-        res.body.status.should.equal('success')
-        res.body.redirect.should.equal('/employees')
-        resolve({ status: 'pass' })
-      })
-  }))
-}
-let loadCurrentModule = function (data) {
-  return (promise = new Promise((resolve, reject) => {
-    if (data.status == 'pass') {
-      server
-        .get('/employees')
-        .expect('Content-type', /text\/html/)
-        .expect(200)
-        .end(function (err, res) {
-          if (err) {
-            console.log(err)
-            reject(err)
-          }
-
-          tokenvalEval = res.headers['x-token']
-
-          resolve({ status: 'pass' })
-        })
-    } else {
-      resolve('fail')
-    }
-  }))
+let testbase = {
+  evalModulename: 'employees',
 }
 
+let genSpecs = require('./Generic.spec.js')
+genSpecs.setevalModulename(testbase.evalModulename)
 describe('Begin Tests', function () {
   // #1 should return home page
   before(function (done) {
-    
-      loginsuccess()
-        .then(loadCurrentModule)
-        .then(function (data) {
-    console.log("****************login and loaded the module  successfully****************")       
-          done()
-        })
-    
+    genSpecs
+      .loginsuccess()
+      .then(genSpecs.loadCurrentModule)
+      .then(function (data) {
+        testbase.tokenvalEval = data
+        console.log(
+          '****************login and loaded the module  successfully****************'
+        )
+        done()
+      })
   })
 
   after(function (done) {
-    server.get('/logout').end(function (err, res) {
+    genSpecs.server.get('/logout').end(function (err, res) {
       if (err) return done(err)
-      console.log("****************logout successfully****************")
+      console.log('****************logout successfully****************')
       done()
-
     })
   })
   it('Throws Validation Message when Input fields are empty or blank', function (done) {
     //calling ADD api
-    //console.log(tokenvalEval)
-    done()
+
+    genSpecs.server
+      .post('/' + evalModulename + genSpecs.dep.searchtype[0])
+      .send(genSpecs.loadModulePayLoad)
+      .set('x-access-token', testbase.tokenvalEval)
+      .expect('Content-type', /json/)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) {
+          done(err)
+          //resolve({ status: 'fail', content: err })
+        } else {
+          console.log(res.body)
+          done()
+        }
+      })
   })
 })
---
+++ app/utils/test/Generic.spec.js
@@ -0,0 +1,78 @@
+var supertest = require('supertest')
+var should = require('chai').should()
+let tokenvalEval
+let loadModulePayLoad = {
+  searchparam: ['NA'],
+  daterange: {
+    startdate: '1982-12-30',
+    enddate: '2019-01-29'
+  },
+  colsearch: 'createdAt',
+  datecolsearch: 'birth_date',
+  pageno: 0,
+  pageSize: 20
+}
+before(async () => {
+  require('../app2.js')
+})
+//./utils/dependentVariables
+let dep = require('../../../app/routes/utils/dependentVariables').routeUrls
+
+// This agent refers to PORT where program is running.
+
+var server = supertest.agent('http://localhost:3011')
+
+// UNIT test begin
+
+let loginsuccess = function (
+  cred = { username: 'krennic', password: 'orson' }
+) {
+  return (promise = new Promise((resolve, reject) => {
+    var user = cred
+    server
+      .post('/login')
+      .send(user)
+      .expect('Content-type', /json/)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) {
+          resolve({ status: 'fail', content: err })
+        }
+
+        res.body.status.should.equal('success')
+        res.body.redirect.should.equal('/' + evalModulename)
+        resolve({ status: 'pass' })
+      })
+  }))
+}
+let setevalModulename = function (data) {
+  evalModulename = data
+}
+let loadCurrentModule = function (data) {
+  return (promise = new Promise((resolve, reject) => {
+    if (data.status == 'pass') {
+      server
+        .get('/' + evalModulename)
+        .expect('Content-type', /text\/html/)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) {
+            console.log(err)
+            reject(err)
+          }
+
+          resolve(res.headers['x-token'])
+        })
+    } else {
+      resolve('fail')
+    }
+  }))
+}
+module.exports = {
+  dep,
+  server,
+  loginsuccess,
+  loadCurrentModule,
+  loadModulePayLoad,
+  setevalModulename
+}
