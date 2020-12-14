11ec92bac15f88f370ee8855b22f992012912881
Rizwan Patel <captainprice212@gmail.com>
2020-12-15 01:14:12 +0530

    updated app startup script for swagger

--- app/utils/app.js
+++ app/utils/app.js
@@ -65,7 +65,7 @@ var express = require("express");
 var Swaggerapp = express();
 const swaggerSpec = require('./configuration/swagger');
 const swaggerUi = require("swagger-ui-express");
-Swaggerapp.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+Swaggerapp.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 Swaggerapp.listen(3012, function () {
   console.log("Express server listening on port 3012");
 });
