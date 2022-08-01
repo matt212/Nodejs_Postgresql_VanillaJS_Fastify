b754264e375af131357758814360e44347b9a871
matt212 <captainprice212@gmail.com>
2022-08-02 00:43:46 +0530

    updated swagger config

--- app/utils/Enable-Swagger.js
+++ app/utils/Enable-Swagger.js
@@ -4,6 +4,7 @@ var Swaggerapp = express();
 const swaggerSpec = require("./configuration/swagger");
 const swaggerUi = require("swagger-ui-express");
 Swaggerapp.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+require('./app')
 Swaggerapp.listen(3013, function () {
   console.log("Swagger Server listening on  http://localhost:3013/api-docs/");
 });
